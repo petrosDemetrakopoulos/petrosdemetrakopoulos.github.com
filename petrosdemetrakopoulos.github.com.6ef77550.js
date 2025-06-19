4a1593f432cfd124faac86c0d54b47018cf04258
Petros <psdeme@hotmail.com>
2025-06-19 11:12:45 +0300

    Updates

---
+++ petrosdemetrakopoulos.github.com.6ef77550.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){setTimeout(function(){let e=document.getElementById("hero");e&&e.querySelectorAll(".flex.flex-wrap.justify-center.gap-4.mb-12 a").forEach(e=>{e.style.color="white"})},500)});
+//# sourceMappingURL=petrosdemetrakopoulos.github.com.6ef77550.js.map
